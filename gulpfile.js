217a1d08246d45aabe63bbcf5207a46a7146df99
Silvia Sanjosé <sanjose.silvia@gmail.com>
2021-04-07 12:55:09 +0200

    include sass

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+ 
+gulp.task('sass', function () {
+
+  return gulp.src('./sass/**/*.scss')
+
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./static'))
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
+});
+
+gulp.task('default', gulp.parallel('sass:watch'));
