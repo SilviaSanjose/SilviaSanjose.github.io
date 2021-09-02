c95c88c65abc67643e8544ed5bc2314966b0714b
Silvia Sanjose <sanjose.silvia@gmail.com>
2021-09-02 19:07:30 +0200

    package.json new photos and js languaje

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
- 
-gulp.task('sass', function () {
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+var sourcemaps = require("gulp-sourcemaps");
 
-  return gulp.src('./sass/**/*.scss')
+gulp.task("sass", function () {
+  return gulp
+    .src("./sass/**/*.scss")
 
     .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./static'))
+    .pipe(gulp.dest("./static"));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', gulp.parallel('sass'));
+gulp.task("sass:watch", function () {
+  gulp.watch("./sass/**/*.scss", gulp.parallel("sass"));
 });
 
-gulp.task('default', gulp.parallel('sass:watch'));
+gulp.task("default", gulp.parallel("sass:watch"));
--- static/animation.js
+++ static/animation.js
@@ -1,19 +1,18 @@
 function setStyles(name, time) {
-    let elementos = document.getElementsByClassName(name);
-    console.log(elementos);
-    setTimeout(function(){ 
-        for (var i = 0; i < elementos.length; i++ ) {
-            elementos[i].style.cssText = "opacity: 1;transform: scale(1);";
-        }
-    }, time);
+  let elementos = document.getElementsByClassName(name);
+  console.log(elementos);
+  setTimeout(function () {
+    for (var i = 0; i < elementos.length; i++) {
+      elementos[i].style.cssText = "opacity: 1;transform: scale(1);";
+    }
+  }, time);
 }
 
-setStyles('python', 1500);
-setStyles('css', 2000);
-setStyles('flask', 2500);
-setStyles('django', 3000);
-setStyles('html', 3500);
-setStyles('sql', 4000);
-setStyles('java', 4500);
-setStyles('javascript', 5000);
-
+setStyles("python", 1500);
+setStyles("css", 2000);
+setStyles("flask", 2500);
+setStyles("django", 3000);
+setStyles("html", 3500);
+setStyles("sql", 4000);
+setStyles("java", 4500);
+setStyles("js", 5000);
